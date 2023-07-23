8f770a450ef457ec52a356d3162c9175ac9b0c86
laurenisgod <140354348+laurenisgod@users.noreply.github.com>
2023-07-24 02:00:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ client.on('warn', e => {
 client.on('error', e => {
     console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
-client.login(ayarlar.token);
+client.login(process.env.token);
 
 //---------------------------------------------------KOMUTLAR--------------------------------------------------- //
 
